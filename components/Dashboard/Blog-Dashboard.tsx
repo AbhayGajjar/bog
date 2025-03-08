c224bcca346de552d5f2bfb07528a2cd262ce5a2
AbhayGajjar <abhaymgajjar@gmail.com>
2025-03-08 07:47:51 +0530

    working on the dashboard

--- components/Dashboard/Blog-Dashboard.tsx
+++ components/Dashboard/Blog-Dashboard.tsx
@@ -1,7 +1,15 @@
 import Link from "next/link";
 import React from "react";
 import { Button } from "../ui/button";
-import { Plus, PlusCircle } from "lucide-react";
+import { FileText, MessageCircle, Plus, PlusCircle } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
 
 const BlogDashboard = () => {
   return (
@@ -21,6 +29,47 @@ const BlogDashboard = () => {
             New Article
           </Button>
         </Link>
+      </div>
+
+      {/* cards */}
+
+      <div className="grid md:grid-cols-3 gap-3 mb-8">
+        <Card>
+          <CardHeader className="flex flex-row justify-between items-center space-y-0 ">
+            <CardTitle>Total Article</CardTitle>
+            <FileText className="h-5 w-5" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">5</div>
+            <p className="text-sm text-muted-foreground mt-2">
+              +5 from last month
+            </p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row justify-between items-center space-y-0  ">
+            <CardTitle>Total Comments</CardTitle>
+            <MessageCircle className="h-5 w-5" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-semibold">3</div>
+            <p className="text-sm text-muted-foreground mt-2">
+              12 awaited modration
+            </p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row justify-between items-center space-y-0  ">
+            <CardTitle>Ave. Rating Time</CardTitle>
+            <MessageCircle className="h-5 w-5" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-semibold">4.2</div>
+            <p className="text-sm text-muted-foreground mt-2">
+              +0.6 last month
+            </p>
+          </CardContent>
+        </Card>
       </div>
     </main>
   );
